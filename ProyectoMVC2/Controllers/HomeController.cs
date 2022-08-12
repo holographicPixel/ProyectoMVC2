c207f2da71a2dccd1bc91b8004c3e163cffc057c
hologram <redes@redobit.com>
2022-08-12 15:07:15 -0300

    Commir inicial libroVisitas

---
+++ ProyectoMVC2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ProyectoMVC2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoMVC2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+
+        public ActionResult CargaDatos()
+        {
+            // Primero recuperamos los datos ingresados por el visitante en los dos controles HTML:
+            string nombre = Request.Form["nombre"].ToString();
+            string comentarios = Request.Form["comentarios"].ToString();
+            // Luego creamos un objeto de la clase LibroVisitas y llamamos al método Grabar:
+            LibroVisitas libro = new LibroVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+
+        public ActionResult ListadoVisitas()
+        {
+            LibroVisitas libro = new LibroVisitas();
+            string t = libro.Leer();
+            ViewData["libro"] = t;
+            return View();
+        }
+    }
+}
---
+++ ProyectoMVC2/Models/LibroVisitas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace ProyectoMVC2.Models
+{
+    public class LibroVisitas
+    {
+        // Codificamos la clase para permitir almacenar los datos en el archivo de texto:
+        public void Grabar(String nombre, String comentarios)
+        {
+            StreamWriter file = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt", true);
+            file.WriteLine("Nombre: " + nombre + "<br>Comentarios: " + comentarios + "<hr>");
+            file.Close();
+        }
+
+        public string Leer()
+        {
+            StreamReader file = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt");
+            String text = file.ReadToEnd();
+            file.Close();
+            return text;
+        }
+    }
+}
